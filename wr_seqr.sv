ad6bd0bccfc44ed85e329af63e768f4666c6513d
Reddygari Shanmukha reddy <shanmukhareddyshan@gmail.com>
2026-09-21 16:34:39 +0530

    Delete wr_seqr.sv

--- wr_seqr.sv
+++
@@ -1,6 +0,0 @@
-class wr_seqr extends uvm_sequencer #(seq_item);
-`uvm_component_utils(wr_seqr)
-function new(string name="wr_seqr",uvm_component parent);
- super.new(name,parent);
-endfunction
-endclass
